c7164b6578b70afcf943fcdc3608a81bd4aa606f
zmccoll <zachmccoll@gmail.com>
2024-01-08 21:53:54 -0800

    update index.js
    added and required passport, updated request methods to work properly

--- index.js
+++ index.js
@@ -70,8 +70,14 @@ app.use(morgan('common'));
 //using body parser
 app.use(bodyParser.json());
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let auth = require('./auth')(app);
+const passport = require('passport');
+require('./passport');
+
 //list of movies requests
-app.get('/movies', async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.find()
     .then((movies) => {
         res.status(201).json(movies);
@@ -183,7 +189,13 @@ app.get('/users/:Username', async (req, res) => {
 /* expected in JSON format
 { Username: String (required), Password: String (required), Email: String(required), Birthday: Date}
 */
-app.put('users/:Username', async (req, res) => {
+app.put('/users/:Username', async (req, res) => {
+    
+    //condition to check
+    /*if(req.user.Username !== req.params.Username) {
+        return res.status(400).send('Permission Denied');
+    }
+    */
     await Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
     {
         Username: req.body.Username,
@@ -203,9 +215,9 @@ app.put('users/:Username', async (req, res) => {
 });
     
 //Add a movie to user's list of favorites
-app.post('/users/:Username/movies/:MovieID', async (req, res) => {
+app.post('/users/:Username/movies/:MovieId', async (req, res) => {
     await Users.findOneAndUpdate({ Username: req.params.Username }, {
-        $push: { FavoriteMovies: req.params.MovieID }
+        $push: { FavoriteMovies: req.params.Title }
     },
     { new: true }) //this line makes sure that the updated document is returned
     .then((updatedUser) => {
@@ -218,9 +230,9 @@ app.post('/users/:Username/movies/:MovieID', async (req, res) => {
 });
 
 //Deleting a movie from favorite movie list
-app.delete('/users/:Username/movies/:MovieID', async (req, res) => {
+app.delete('/users/:Username/movies/:MovieId', async (req, res) => {
     await Users.findOneAndUpdate({ Username: req.params.Username }, {
-        $pull:  {FavoriteMovies: req.params.MovieID },
+        $pull:  {FavoriteMovies: req.params.Title },
         },
         { new: true }) //this line makes sure updated document is returned
         .then((updatedList) => {
@@ -234,7 +246,7 @@ app.delete('/users/:Username/movies/:MovieID', async (req, res) => {
 
 // Delete a user by username
 app.delete('/users/:Username', async (req, res) => {
-    await Users.findOneAndRemove({ Username: req.params.Username })
+    await Users.findOneAndDelete({ Username: req.params.Username })
         .then((user) => {
             if(!user) {
                 res.status(400).send(req.params.Username + ' was not found');
