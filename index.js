688dfd40bb454cfa79a6fa97dc91961dd6dd354c
zmccoll <zachmccoll@gmail.com>
2024-01-23 14:41:13 -0800

    updat index.js
    added space for user exist error

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ app.post('/users', async (req, res) => {
     await Users.findOne({ Username: req.body.Username })
     .then((user) => {
         if (user) {
-            return res.status(400).send(req.body.Username + 'already exists');
+            return res.status(400).send(req.body.Username + ' already exists');
         } else {
             Users
                 .create({
